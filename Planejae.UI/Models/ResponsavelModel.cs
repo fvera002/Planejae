f126723e1e1f745830a591fb8e22e9365ad3ea12
Fernando Verago <fernandoverago@hotmail.com>
2016-09-12 20:59:27 -0300

    Arquivos não commitados

---
+++ Planejae.UI/Models/ResponsavelModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Planejae.UI.Models
+{
+    [Serializable]
+    public class ResponsavelModel
+    { 
+//verago aqui
+        [Required]
+        public int Id { get; set; }
+    }
+}
